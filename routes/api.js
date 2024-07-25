830b50b1e7746e2a3c5cea617ba00455a79fea27
E-K8 <ekatplat@gmail.com>
2024-07-25 21:39:03 +0100

    fix region placement error response, refactor

--- routes/api.js
+++ routes/api.js
@@ -12,53 +12,57 @@ module.exports = function (app) {
       return res.json({ error: 'Required field(s) missing' });
     }
 
-    if (solver.validate(puzzle) !== 'Valid') {
-      return res.json({ error: solver.validate(puzzle) });
+    const validationError = solver.validate(puzzle);
+    if (validationError !== 'Valid') {
+      return res.json({ error: validationError });
     }
 
-    const row = coordinate.split('')[0];
-    const column = coordinate.split('')[1];
+    const row = coordinate[0];
+    const column = coordinate[1];
     if (
       coordinate.length !== 2 ||
       !/[a-i]/i.test(row) ||
-      !/[1-9]/i.test(column)
+      !/[1-9]/.test(column)
     ) {
-      console.log('invalid coordinate: ');
-      return res.json({ json: 'Invalid coordinate' });
+      return res.json({ error: 'Invalid coordinate' });
     }
 
     if (!/^[1-9]$/.test(value)) {
       return res.json({ error: 'Invalid value' });
     }
 
-    let index = (solver.letterToNumber(row) - 1) * 9 + (+column - 1);
-    if (puzzle[index] == value) {
+    const rowIndex = solver.letterToNumber(row);
+    const columnIndex = parseInt(column);
+    const puzzleIndex = (rowIndex - 1) * 9 + (columnIndex - 1);
+
+    if (puzzle[puzzleIndex] == value) {
       return res.json({ valid: true });
     }
 
-    let validCol = solver.checkColPlacement(puzzle, row, column, value);
-    let validReg = solver.checkRegionPlacement(puzzle, row, column, value);
-    let validRow = solver.checkRowPlacement(puzzle, row, column, value);
-    let conflicts = [];
+    const validRow = solver.checkRowPlacement(puzzle, row, column, value);
+    const validCol = solver.checkColPlacement(puzzle, row, column, value);
+    const validReg = solver.checkRegionPlacement(puzzle, row, column, value);
 
-    if (validCol && validReg && validRow) {
-      res.json({ valid: true });
+    const conflict = [];
+    if (!validRow) conflict.push('row');
+    if (!validCol) conflict.push('column');
+    if (!validReg) conflict.push('region');
+
+    if (validRow && validCol && validReg) {
+      return res.json({ valid: true });
     } else {
-      if (!validRow) {
-        conflicts.push('row');
-      }
-      if (!validCol) {
-        conflicts.push('column');
-      }
+      return res.json({ valid: false, conflict });
     }
   });
 
   app.route('/api/solve').post((req, res) => {
     const puzzle = req.body.puzzle;
-    if (solver.validate(puzzle) !== 'Valid') {
-      return res.json({ error: solver.validate(puzzle) });
+    const validationError = solver.validate(puzzle);
+    if (validationError !== 'Valid') {
+      return res.json({ error: validationError });
     }
-    const solution = solver.completeSudoku(puzzle);
+
+    const solution = solver.solve(puzzle);
     if (solution.error) {
       return res.json(solution);
     }
