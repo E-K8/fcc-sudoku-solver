a7ec4ec763f39b7fe29dfb0cceddb1a1a3d1c4e7
E-K8 <ekatplat@gmail.com>
2024-07-26 23:28:21 +0100

    test that logic handles a valid puzzle string of 81 characters

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,8 +2,18 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver();
+
+let validPuzzle =
+  '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
 
 suite('Unit Tests', () => {
-
+  suite('solver tests', function () {
+    test('Logic handles a valid puzzle string of 81 characters', function (done) {
+      let complete =
+        '135762984946381257728459613694517832812936745357824196473298561581673429269145378';
+      assert.equal(solver.solve(validPuzzle), complete);
+      done();
+    });
+  });
 });
