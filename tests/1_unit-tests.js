da8872d73c56d4a946716708a9476c3f01e8a576
E-K8 <ekatplat@gmail.com>
2024-07-27 21:39:37 +0100

    test a puzzle string that is not 81 characters in length

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -22,5 +22,12 @@ suite('Unit Tests', () => {
       assert.equal(solver.solve(invalidPuzzle), false);
       done();
     });
+
+    test('Logic handles a puzzle string that is not 81 characters in length', function (done) {
+      let invalidPuzzle =
+        '2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.';
+      assert.equal(solver.solve(invalidPuzzle), false);
+      done();
+    });
   });
 });
