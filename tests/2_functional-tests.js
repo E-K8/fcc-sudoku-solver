e2ced12e03b6269ab7cd1bd3fdabcddfde86152d
E-K8 <ekatplat@gmail.com>
2024-08-04 20:46:38 +0100

    functional test - Solve a puzzle with valid puzzle string

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,4 +1,4 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
@@ -6,6 +6,23 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+  suite('POST /api/solve', () => {
+    test('Solve a puzzle with valid puzzle string', (done) => {
+      chai
+        .request(server)
+        .post('/api/solve')
+        .send({
+          puzzle:
+            '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(
+            res.body.solution,
+            '135762984946381257728459613694517832812936745357824196473298561581673429269145378'
+          );
+          done();
+        });
+    });
+  });
 });
-
