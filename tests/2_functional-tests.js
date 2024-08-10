0473af16e0deda2bbe58b483dd8505eb1d6b8d66
E-K8 <ekatplat@gmail.com>
2024-08-10 22:11:36 +0100

    functional test - Check a puzzle placement with multiple placement conflicts

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -122,5 +122,25 @@ suite('Functional Tests', () => {
           done();
         });
     });
+
+    test('Check a puzzle placement with multiple placement conflicts', (done) => {
+      chai
+        .request(server)
+        .post('/api/check')
+        .send({
+          puzzle:
+            '1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.',
+          coordinate: 'A2',
+          value: '2',
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.isFalse(res.body.valid);
+          assert.isArray(res.body.conflict);
+          assert.include(res.body.conflict, 'row');
+          assert.include(res.body.conflict, 'column');
+          done();
+        });
+    });
   });
 });
